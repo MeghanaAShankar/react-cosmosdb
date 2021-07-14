6d3b0c8ff443585cc8bf61674bb29d517707af14
Meghana <meghananikhi@gmail.com>
2021-07-14 17:11:30 +0530

    new

---
+++ server/env/environment.js
@@ -0,0 +1,9 @@
+const dbName = "meghana";
+const key =
+  "6p7jDiZaNT8l1WSuz2NIvSLzOeVlrDgkeDdwLhYeVFy56JYO3hT2RgCcCYfbzb8xTGXukkFnXxFgabjU2p7tKA==";
+const cosmosPort = 10255;
+module.exports = {
+  cosmosPort,
+  dbName,
+  key,
+};
--- server/mongo.js
+++ server/mongo.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const env = require('./env/environment');
+const mongoose = require("mongoose");
+const env = require("./env/environment");
 
 mongoose.Promise = global.Promise;
 
 const mongoUri = `mongodb://${env.dbName}.documents.azure.com:${env.cosmosPort}/?ssl/=true`;
 
 function connect() {
-  return mongoose.connect(mongoUri, { auth: { user: env.dbName, password: env.key }});
+  return mongoose.connect(mongoUri, { useMongoClient: true });
 }
 
 module.exports = {
   connect,
-  mongoose
+  mongoose,
 };
